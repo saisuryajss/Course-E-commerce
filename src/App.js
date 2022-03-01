751b778ea4b201122da980215e7f1143db7b42d4
saisuryajss <saisuryajss@gmail.com>
2022-03-01 20:04:15 +0530

    minor changes with redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 
-import { Routes , Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
 import SignInSignUp from './pages/signin-signup/SignInSignUp';
-import {useState,useEffect} from 'react';
-import {onAuthChange} from './firebase/firebase';
+import { useState, useEffect } from 'react';
+import { onAuthChange } from './firebase/firebase';
 
 function App() {
-  let [user,setUser]=useState(null);
+  let [user, setUser] = useState(null);
 
-  useEffect(()=>{
-    const handleLogin=onAuthChange(setUser);
-    return ()=>handleLogin();
-  },[]);
+  useEffect(() => {
+    const handleLogin = onAuthChange(setUser);
+    return () => handleLogin();
+  }, []);
 
   return (
     <div>
       <Header currentUser={user} />
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/shop' element={<ShopPage />} />
-      <Route path='/login' element={<SignInSignUp />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/login' element={<SignInSignUp />} />
+      </Routes>
     </div>
   );
 }
