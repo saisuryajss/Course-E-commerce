4be008c9ca3ab133fd1286e56af6127e0efca409
saisuryajss <saisuryajss@gmail.com>
2022-03-04 12:22:30 +0530

    minor changes related to add-to-cart button

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -3,41 +3,46 @@ import './CollectionItem.css';
 import CustomButton from '../custom-button/CustomButton';
 
 function CollectionItem({ name, imageUrl, price }) {
-   const handleOnMouseEnter=(event)=>{
-    if(event.target.children[0]){
-        event.target.children[0].style.display='flex';
-        event.target.children[0].opacity='0.85'
+    const handleOnMouseEnter = (event) => {
+        const target = event.target.children[0];
+        if (target) {
+            target.style.display = 'flex';
+            target.style.opacity = '0.85'
+        }
     }
-}
 
-   const handleOnMouseLeave=(event)=>{
-    if(event.target.children[0]){
-        event.target.children[0].style.display='none';
-        event.target.children[0].opacity='0.70';
+    const handleOnMouseLeave = (event) => {
+        const target = event.target.children[0];
+        if (target) {
+            target.style.display = 'none';
+            target.opacity = '0.70';
+        }
     }
-}
 
-const handleCartMouseOver=(event)=>{
-    if(event.target){
-        event.target.style.backgroundColor='black';
-        event.target.style.color='white';
+    const handleCartMouseOver = (event) => {
+        const target = event.target;
+        if (target) {
+            target.style.backgroundColor = 'black';
+            target.style.color = 'white';
+            target.style.border = '2px solid black';
+        }
     }
-}
 
-   const handleCartMouseOut=(event)=>{
-    if(event.target){
-        event.target.style.backgroundColor='white';
-        event.target.style.color='black';
+    const handleCartMouseOut = (event) => {
+        const target = event.target;
+        if (target) {
+            target.style.backgroundColor = 'white';
+            target.style.color = 'black';
+        }
     }
-}
 
 
     return (
-        <div className='collection-item' onMouseEnter={handleOnMouseEnter}  onMouseLeave={handleOnMouseLeave}>
+        <div className='collection-item' onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave}>
             <div
                 className='collection-image'
                 style={{ backgroundImage: `url(${imageUrl})` }}>
-                <div className='add-to-cart' onMouseOver={handleCartMouseOver}  onMouseOut={handleCartMouseOut}>
+                <div className='add-to-cart' onMouseOver={handleCartMouseOver} onMouseOut={handleCartMouseOut}>
                     <CustomButton type='button' size='button-large' inverted={true} > ADD TO CART </CustomButton>
                 </div>
             </div>
